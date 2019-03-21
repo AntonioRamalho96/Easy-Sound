8086b8116aaf10fc50539ea844899fe43df0ce1b
AntonioRamalho96 <39343934+AntonioRamalho96@users.noreply.github.com>
2019-03-21 14:45:06 -0700

    commented

--- EXAMPLES/playNotes.py
+++ EXAMPLES/playNotes.py
@@ -4,38 +4,40 @@ import struct
 import matplotlib.pyplot as plt
 import copy
 
+#returns a record object with frequency -freq- in Hz, volume -Volume- (from 0 to 32000)
+#and -time- seconds of lenght
 def produceTone(record, freq, Volume, time):
     frames=[]
     plotFrames=()
     count=0
-    for i in range(ES.timeToChunks(time)):
+    for i in range(ES.timeToChunks(time)): #for all the chunks of the record
         thisChunk=()
-        for j in range(1024):
-            thisChunk=thisChunk+(((np.int16)(Volume*np.cos(freq*np.pi*2/44100*count))),)  #could be done more efficiently
+        for j in range(1024): #for each of the samples in each chunk
+            thisChunk=thisChunk+(((np.int16)(Volume*np.cos(freq*np.pi*2/44100*count))),)  #put samples representing a cosine function
             count=count+1
         frames.append(struct.pack("=1024h", *thisChunk))
         plotFrames=plotFrames+thisChunk
     record.setFrames(frames)
-    #record.playRecord()
     return copy.copy(record)
-    #plt.plot(plotFrames)
-    #plt.show()
 
+#creates a Record object
 print('openning stream...')
 ES=EasySound()
 S=ES.openStream()
 rec=Record(S)
 
+#create Record Objects for each tone
 input('Done! Press ENTER to load notes')
-G=produceTone(rec, 392, 5000, 0.5)
-A=produceTone(rec, 440, 5000.0, 0.5)
-A2=produceTone(rec, 440, 5000.0, 1)
+G=produceTone(rec, 392, 5000, 0.5) #G tone
+A=produceTone(rec, 440, 5000.0, 0.5) #A tone
+A2=produceTone(rec, 440, 5000.0, 1) #A tone with twice the lenght
 B=produceTone(rec, 493, 5000, 0.5)
 B2=produceTone(rec, 493, 5000, 1)
 C=produceTone(rec, 523, 5000, 0.5)
 D=produceTone(rec, 587, 5000, 0.5)
-P=produceTone(rec, 0, 0, 0.5)
+P=produceTone(rec, 0, 0, 0.5) #pause(record with volume=0)
 
+#Plays ode of joy!
 input('Done! Press ENTER to play song')
 B.playRecord()
 B.playRecord()
