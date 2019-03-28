4b6c4131d1c2f53bb60546c1e0e0b8baceb84b35
AntonioRamalho96 <39343934+AntonioRamalho96@users.noreply.github.com>
2019-03-27 20:27:14 -0700

    Correscte song times :p

--- EXAMPLES/playNotes.py
+++ EXAMPLES/playNotes.py
@@ -9,8 +9,9 @@ input('Done! Press ENTER to load notes')
 G=ES.produceTone(stream, 392, 5000, 0.5) #G tone
 A=ES.produceTone(stream, 440, 5000.0, 0.5) #A tone
 A2=ES.produceTone(stream, 440, 5000.0, 1) #A tone with twice the lenght
+A05=ES.produceTone(stream, 440, 5000.0, 0.25) #A tone with 0.5 the lenght
 B=ES.produceTone(stream, 493, 5000, 0.5)
-B2=ES.produceTone(stream, 493, 5000, 1)
+B15=ES.produceTone(stream, 493, 5000, 0.75)
 C=ES.produceTone(stream, 523, 5000, 0.5)
 D=ES.produceTone(stream, 587, 5000, 0.5)
 P=ES.produceTone(stream, 0, 0, 0.5) #pause(record with volume=0)
@@ -29,7 +30,6 @@ G.playRecord()
 G.playRecord()
 A.playRecord()
 B.playRecord()
-B.playRecord()
-P.playRecord()
-A.playRecord()
+B15.playRecord()
+A05.playRecord()
 A2.playRecord()
